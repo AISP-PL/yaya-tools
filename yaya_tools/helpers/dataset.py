5a7460fd95dcffa02fb27f9a490c393e1d6c8645
Sławomir Paszko <folkus@gmail.com>
2025-01-30 00:43:46 +0100

    Fixes.

--- yaya_tools/helpers/dataset.py
+++ yaya_tools/helpers/dataset.py
@@ -2,9 +2,6 @@
 This module contains helper functions for dataset management
 """
 
-import os
-from typing import Optional
-
 
 def dataset_to_validation() -> None:
     """Create a validation.txt file from the dataset folder"""
